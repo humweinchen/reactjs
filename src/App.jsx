67516758bca9f2176d00dc4ac2af2f65f836025f
Jackson Hum <101075194+humweinchen@users.noreply.github.com>
2023-11-18 11:41:03 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { TodoForm } from '../TodoList/Components/TodoForm'
+import './App.css'
+
+
+
+function App() {
+  return <TodoForm /> 
+
+}
+
+export default App
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,14 @@
+//the shortcut key to import the function is "rfc", then enter.
+
+import React from 'react'
+import '/.hello.css'
+
+export default function Hello() {
+  return (
+    <div>Eyyo Coach
+        <>Eyoo Brooooooo</>
+
+    </div>
+    
+  )
+}
